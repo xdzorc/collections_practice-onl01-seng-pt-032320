77e455e2206d9870ef8ae0810da03670b4266ef0
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 07:14:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+   if a<b
+    1
+   else
+    0 
+   end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do|a,b|
+    if a.length>b.length
+      1 
+    else
+      0 
+    end
+  end
+end
+  
+  
+  def swap_elements(array)
+    a=array[1]
+    array[1]=array[2]
+    array[2]=a
+    array
+  end
+  
+  
